21661779b85bf084806c55a1608432b7647ea143
hessika <jchen6038@gmail.com>
2025-04-21 23:24:20 -0700

    lab 3

---
+++ lab3/js/tyfu.js
@@ -0,0 +1,16 @@
+
+//Lets pseudocode this thing: i want to add a link from my lab 3 page to my overall homepage
+
+//figure our the link
+
+//create a section on our webpage for putting a link
+
+//coding the linking
+
+//save changes
+
+//tetsing how it works
+
+//publish
+
+//test in github

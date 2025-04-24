21be893a8f6d2a31a27bbae58471953c750f01eb
hessika <jchen6038@gmail.com>
2025-04-24 10:10:07 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -7,15 +7,24 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Create a variable called carName and assign the value "Taurus" to it, then print it.
+let carName = "Tesla Model Y";
+console.log("Make: " + carName);
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Create a variable called carYear and assign the value to 1995 to it and display it.
+let carYear = 2024;
+console.log("Year:", carYear);
 
-// let's get this party started
-main();
+// Create and assign a variable called currentYear
+let currentYear = 2025;
+
+// Assign carAge to the difference between currentYear and carYear and display it.
+let carAge = currentYear - carYear;
+console.log ("Age:", carAge);
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Make: " + Model + "<br>");
+document.writeln("Make: " + color + "<br>");
+document.writeln("Make: " + year + "<br>");
+document.writeln("Make: " + age + "years<br>");

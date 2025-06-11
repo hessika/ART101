35ab771f8091f9397d9fa53cb377510a5da1cfe8
hessika <jchen6038@gmail.com>
2025-06-10 23:53:49 -0700

    finish

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,36 +1,22 @@
-// function main() {
-//     console.log("Main function started.");
-//     // the code that makes everything happen
-// }
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+    }
 
-// // let's get this party started
-// main();
 
-/*
-Lab.js - This simple JavaScript/jQuery script uses button to modify same elements on the page.
+// click listener for button
+$("#make-convo").click(function(){
 
-Requirements: jQuery must be loaded for this scrip to work.
+    // get new fake dialogue
+const newText = generateRandomText();
 
-Author: Jessica Chen
-Date: May 12 202
-*/
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
 
-// Add buttons to each section
-$(document).ready(function() {
-    $("#challenge").append("<button id='button-challenge'>challenge</button>");
-    $("#problems").append("<button id='button-problems'>problems</button>");
-    $("#results").append("<button id='button-results'>results</button>");
-
-    // Add click listeners to each button
-    $("#button-challenge").click(function(){
-        $("#challenge").toggleClass("special");
-    });
-
-    $("#button-problems").click(function(){
-        $("#problems").toggleClass("special");
-    });
-
-    $("#button-results").click(function(){
-        $("#results").toggleClass("special");
-    });
 });
